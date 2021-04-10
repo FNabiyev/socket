8e5cd993abe6bff8e62113ea32fcc34118e5f1e4
Fazlidddin <nabiyevfazliddin17@gmail.com>
2021-04-10 17:47:44 +0500

    10.04

--- main/routing.py
+++ main/routing.py
@@ -3,7 +3,5 @@ from .consumers import *
 
 ws_urlpatterns = [
     path('ws/book/', BookSocket.as_asgi()),
-    path('ws/chatting/', ChattingSocket.as_asgi()),
-    path('ws/booking/', BookingSocket.as_asgi()),
-    path('ws/chatting/<str:room_name>/', ChatingSocket.as_asgi())
+    path('ws/chat/<str:room_name>', ChatConsumer.as_asgi()),
 ]
